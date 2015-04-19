681f93510a7a339810586dca995fde4f64146ee8
Alex <almoav@gmail.com>
2015-04-18 17:07:18 -0700

    add map

--- js/helper.js
+++ js/helper.js
@@ -104,20 +104,21 @@ Start here! initializeMap() is called when page is loaded.
 function initializeMap() {
 
   var locations;
-
+  
   var mapOptions = {
     disableDefaultUI: true
   };
 
   // This next line makes `map` a new Google Map JavaScript Object and attaches it to
   // <div id="map">, which is appended as part of an exercise late in the course.
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  map = new google.maps.Map(document.querySelector('#mapDiv'), mapOptions);
 
 
   /*
   locationFinder() returns an array of every location string from the JSONs
   written for bio, education, and work.
   */
+  
   function locationFinder() {
 
     // initializes an empty array
@@ -140,12 +141,13 @@ function initializeMap() {
 
     return locations;
   }
-
+  
   /*
   createMapMarker(placeData) reads Google Places search results to create map pins.
   placeData is the object returned from search results containing information
   about a single location.
   */
+  
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
@@ -153,7 +155,7 @@ function initializeMap() {
     var lon = placeData.geometry.location.lng();  // longitude from the place service
     var name = placeData.formatted_address;   // name of the place from the place service
     var bounds = window.mapBounds;            // current boundaries of the map window
-
+    
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
       map: map,
@@ -175,27 +177,31 @@ function initializeMap() {
 
     // this is where the pin actually gets added to the map.
     // bounds.extend() takes in a map location object
+    
     bounds.extend(new google.maps.LatLng(lat, lon));
     // fit the map to the new marker
     map.fitBounds(bounds);
     // center the map
     map.setCenter(bounds.getCenter());
+    
   }
-
   /*
   callback(results, status) makes sure the search returned results for a location.
   If so, it creates a new map marker for that location.
   */
+  
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
     }
   }
 
+  
   /*
   pinPoster(locations) takes in the array of locations created by locationFinder()
   and fires off Google place searches for each location
   */
+  
   function pinPoster(locations) {
 
     // creates a Google place search service object. PlacesService does the work of
@@ -215,7 +221,7 @@ function initializeMap() {
       service.textSearch(request, callback);
     }
   }
-
+  
   // Sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
 
@@ -232,12 +238,13 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
+
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+// Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
